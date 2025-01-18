a1687f5f70e561b13c56c59369faa6dff07481f2
bokubeam <bokubeam@gmail.com>
2025-01-17 21:23:17 -0700

    Make ball start at a random angle after a pause

--- ball.gd
+++ ball.gd
@@ -3,15 +3,19 @@ extends CharacterBody2D
 signal player_1_scored
 signal player_2_scored
 
+var startpos: Vector2
+var speed_vel: Vector2 = Vector2(0, 0)
+var ball_speed = 20.0
+
 func _ready()->void:
 	ready_ball()
-	
-var startpos : Vector2
-var speed_vel :Vector2 = Vector2(-10,5)
+
 func ready_ball():
 	position =  $"../Camera2D".get_screen_center_position()
+	speed_vel = Vector2.ZERO
 	await get_tree().create_timer(0.2).timeout
-	speed_vel += Vector2(randf_range(0,1),randf_range(0,1))
+	var random_angle = randf_range(315, 405)
+	speed_vel = Vector2.from_angle(deg_to_rad(random_angle)) * ball_speed
 	
 func _physics_process(delta: float) -> void:
 	velocity = speed_vel
